3b5260e8b9a195edd3076cceb2f6710d872e90d6
gnocchi <gkussunoki@gmail.com>
2023-03-29 17:34:56 -0300

    Add painting functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('.container');
 container.classList.add('square');
 
+function highlightDiv(event) {
+    this.classList.add('highlight');
+}
+
+// adding a 16x16 grid of divs into the DOM
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -13,3 +18,6 @@ for (let i = 0; i < 16; i++) {
 
     container.appendChild(row);
 }
+
+const tiles = document.querySelectorAll('.row > div');
+tiles.forEach(div => div.addEventListener('mouseleave', highlightDiv));
