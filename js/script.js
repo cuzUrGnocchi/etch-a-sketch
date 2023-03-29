a91ec5b1aec3acf2a83f9f61f029d883b1da34d0
gnocchi <gkussunoki@gmail.com>
2023-03-29 17:07:57 -0300

    Make a 16x16 grid of divs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const container = document.querySelector('.container');
+container.classList.add('square');
 
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        row.appendChild(div);
+        div.classList.add('square');
+    }
+
+    container.appendChild(row);
 }
