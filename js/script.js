213009afb9c8982c5bbce9bb40c1fec3fd230cbf
gnocchi <gkussunoki@gmail.com>
2023-03-30 15:59:27 -0300

    Make colors random and add shades to tiles that get repainted

--- js/script.js
+++ js/script.js
@@ -1,23 +1,73 @@
-const container = document.querySelector('.container');
-container.classList.add('square');
+const INITIAL_GRID_SIZE = 16; 
+const body = document.querySelector('body');
+const button = document.querySelector('button');
 
-function highlightDiv(event) {
-    this.classList.add('highlight');
+generateGrid(INITIAL_GRID_SIZE);
+button.addEventListener('click', generateGrid);
+
+function highlightDiv() {
+    if (!this.classList.contains('paint')) {
+        this.classList.add('highlight');
+    }
 }
 
-// adding a 16x16 grid of divs into the DOM
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+function paintDiv() {
+    const currentColor = window.getComputedStyle(this).backgroundColor.match(/\d[^\)]*/)[0].replace(' ', '');
+    let baseColor = this.dataset.baseColor;
+    let colorToApply; 
 
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        row.appendChild(div);
-        div.classList.add('square');
+    if (!baseColor) {
+        colorToApply = getRandomColor();
+        this.dataset.baseColor = colorToApply;
+    } else {
+        colorToApply = getDarkerColor(currentColor, baseColor);
     }
 
-    container.appendChild(row);
+    this.style.backgroundColor = 'rgb(' + colorToApply + ')';
 }
 
-const tiles = document.querySelectorAll('.row > div');
-tiles.forEach(div => div.addEventListener('mouseleave', highlightDiv));
+function generateGrid(size) {
+    let grid = document.querySelector('.grid');
+    if (grid !== null) grid.remove();
+    grid = document.createElement('div');
+    grid.classList.add('grid', 'square');
+    body.appendChild(grid);
+    
+    while (typeof size !== 'number') {
+        size = prompt('What size?');
+    }
+    
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            row.appendChild(div);
+            div.classList.add('square');
+            div.setAttribute('data-base-color', '');
+        }
+        grid.appendChild(row);
+        row.classList.add('row');
+    }
+
+    const tiles = document.querySelectorAll('.row > div');
+    tiles.forEach(div => {
+        div.addEventListener('mouseleave', paintDiv);
+    });
+}
+
+function getDarkerColor(currentColor, baseColor) {
+    const darkerColor = [];
+    for (let i = 0; i < 3; i++) { 
+        const decrement = +baseColor.split(',')[i] * 0.1;
+        darkerColor.push(+currentColor.split(',')[i] - decrement);
+    }
+    return darkerColor.join(',');
+}
+
+function getRandomNumber(limit) {
+    return Math.floor(Math.random() * limit);
+}
+
+function getRandomColor() {
+    return getRandomNumber(256) + ',' + getRandomNumber(256) + ',' + getRandomNumber(256);
+}
